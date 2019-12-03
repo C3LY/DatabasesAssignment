f621ad6bd39a7694881af73f389940aba0c99604
Chloe <cy214@sussex.ac.uk>
2019-12-03 14:50:30 +0000

    q9 version 2works but inefficient

---
+++ q9v2.sql
@@ -0,0 +1,17 @@
+-- list all operations that overlap times
+-- theatre no = theatre
+-- start time - Time1 no date
+-- start time of second op -  time2
+-- if(tbl2.startDateTime <= (addtime(TIME(tbl1.startDateTime), duration))  AND addtime(TIME(tbl2.startDateTime), duration) >= table1.startDateTime)
+-- ((TIME(HO2.startDateTime)) <= (ADDTIME(TIME(HO1.startDateTime),HO1.duration)))
+
+
+SELECT HO1.theatreNo, HO1.startDateTime as starttime1, (addtime(TIME(HO1.startDateTime), HO1.duration)) as endtime1, HO2.startDateTime as starttime2, (addtime(TIME(HO2.startDateTime), HO2.duration)) as endtime2
+FROM Hospital_Operation AS HO1
+    INNER Join Hospital_Operation as HO2 on
+        (HO1.theatreNo = HO2.theatreNo) AND date(HO1.startDateTime) = date(HO2.startDateTime)
+WHERE (DATE(HO1.startDateTime) = DATE(HO2.startDateTime)) AND
+        ((TIME(HO2.startDateTime)) < (addtime(TIME(HO1.startDateTime), HO1.duration)))  AND
+        ((addtime(TIME(HO2.startDateTime), HO2.duration)) >= (TIME(HO1.startDateTime)))
+        AND !((HO1.theatreNo = HO2.theatreNo) AND (HO1.startDateTime = HO2.startDateTime))
+;
