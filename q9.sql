56537e7d568d0ee1ba0255d631abae473bbf5cfd
Chloe <cy214@sussex.ac.uk>
2019-12-03 13:10:20 +0000

    q9 wrong

--- q9.sql
+++ q9.sql
@@ -2,10 +2,13 @@
 -- theatre no = theatre
 -- start time - Time1 no date
 -- start time of second op -  time2
+-- if(tbl2.startDateTime <= (addtime(TIME(tbl1.startDateTime), duration))  AND addtime(TIME(tbl2.startDateTime), duration) >= table1.startDateTime)
 
 
-select tbl2theatreNo, startDateTime, duration, count(date(startDateTime)) as count
-from Hospital_Operation as tble1
-join Hospital_Operation as tble2 ON tble1.theatreNo = tble2.theatreNo
-
-GROUP BY (date(startDateTime))
+select HO1.theatreNo, HO1.startDateTime, HO1.duration
+from Hospital_Operation as HO1
+    Inner Join Hospital_Operation as HO2 on
+        (HO1.theatreNo = HO2.theatreNo) AND
+        (DATE(HO1.startDateTime) = DATE(HO2.startDateTime)) AND
+        (TIME(HO2.startDateTime) <= (addtime(TIME(HO1.startDateTime), HO1.duration)) AND (addtime(TIME(HO2.startDateTime), HO2.duration) >= HO1.startDateTime))
+-- GROUP BY (date(startDateTime))
