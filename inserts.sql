a66be932bb09a2879524b8aad8e5fcee1572c64a
Chloe <cy214@sussex.ac.uk>
2019-12-03 14:26:56 +0000

    inserts to practice

---
+++ inserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO Hospital_Operation (theatreNo, startDateTime, optype, duration, patient)
+VALUES (2, '2016-12-02 9:00', 'ear', '05:00:00', 'AB234CD1X');
+
+INSERT INTO Hospital_Operation (theatreNo, startDateTime, optype, duration, patient)
+VALUES (2, '2016-12-02 11:00', 'eye', '02:10:00', 'ZAS10158U');
+
+
