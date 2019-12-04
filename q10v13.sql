2cec0562764fcafc3c297515d077a238f23b2bcf
Chloe <cy214@sussex.ac.uk>
2019-12-04 01:26:20 +0000

    q10v13 i think it works

--- q10v13.sql
+++ q10v13.sql
@@ -1,11 +1,16 @@
-select t2.*
+select t2.theatreNo,
+       day(t2.date1)       AS day,
+       monthname(t2.date1) AS month,
+       year(t2.date1)      AS year,
+       t2.totalops as numOps
 from (
 select theatreNo, max(totalops) as totalops from (
 select theatreNo, date(startDateTime), count(*) as totalops
   from Hospital_Operation t
   group by theatreNo, date(startDateTime)
 ) s1 group by theatreNo) w join
-(select theatreNo, date(startDateTime), count(*) as totalops
+(select theatreNo, date(startDateTime) as date1, count(*) as totalops
   from Hospital_Operation t
   group by theatreNo, date(startDateTime)
-) t2 on t2.theatreNo = w.theatreNo and t2.totalops = w.totalops;
+) t2 on t2.theatreNo = w.theatreNo and t2.totalops = w.totalops
+ORDER BY theatreNo asc, date1 asc;
